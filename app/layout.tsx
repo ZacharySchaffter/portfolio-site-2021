64e3a4f091c5ed333d70c4aef494ffea14466dc5
Zachary Schaffter <zachary.schaffter@gmail.com>
2024-12-31 08:53:48 -0500

    feat: source page meta from CMS

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.scss";
 import { PropsWithChildren } from "react";
 import { ParallaxProvider } from "@/context/Parallax";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "Zachary Schaffter | %s",
+    default: "Zachary Schaffter | Frontend Software Engineer", // a default is required when creating a template
+  },
+};
 
 const RootLayout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
